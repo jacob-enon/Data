c9a20f8afc4b42fb4765a35fc81f4128efc31b0d
Jacob Enon <jacobenon@hotmail.com>
2020-07-08 21:27:03 +0100

    Initial commit

---
+++ Data/IDeserialiser.cs
@@ -0,0 +1,16 @@
+﻿namespace BigJacob.Data
+{
+    /// <summary>
+    /// Deserialises files
+    /// </summary>
+    public interface IDeserialiser
+    {
+        /// <summary>
+        /// Deserialise a file
+        /// </summary>
+        /// <typeparam name="T"> Type of object to deserialise into </typeparam>
+        /// <param name="path"> Path of file to deserialise </param>
+        /// <returns> Deserialised object </returns>
+        T DeserialiseFile<T>(string path);
+    }
+}
---
+++ Data/JsonDeserialiser.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace BigJacob.Data
+{
+    /// <summary>
+    /// Deserialises JSON files
+    /// </summary>
+    public class JsonDeserialiser : IDeserialiser
+    {
+        /// <summary>
+        /// Deserialise a JSON file
+        /// </summary>
+        /// <typeparam name="T"> Type of object to deserialise into </typeparam>
+        /// <param name="path"> Path of file to deserialise </param>
+        /// <returns> Deserialised object </returns>
+        public T DeserialiseFile<T>(string path) => JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+    }
+}
